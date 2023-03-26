081e149901921a8f8d09387a0f7763b00e4331ff
Shubham Singh <shubham212a@gmail.com>
2023-03-26 17:44:25 +0530

    app.js

--- App.js
+++ App.js
@@ -2,27 +2,23 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 
-import { createAppContainer , createSwitchNavigator} from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 
-import Home from "./screen/Home"
-import Profile from "./screen/Profile"
-import Appointments from "./screen/Appointment"
-import AddAppointments from "./screen/AddAppointment"
-import Patient from "./screen/Patient"
-import AddPatient from "./screen/AddPatient"
-import History from "./screen/History"
-import Critical from "./screen/Critcal"
-import CriticalProfile from "./screen/CriticalProfile"
+import Home from "./screens/Home"
+import Profile from "./screens/Profile"
+import Appointments from "./screens/Appointment"
+import AddAppointments from "./screens/AddAppointment"
+import Patient from "./screens/Patient"
+import AddPatient from "./screens/AddPatient"
+import History from "./screens/History"
+import Critical from "./screens/Critcal"
+import CriticalProfile from "./screens/CriticalProfile"
 
 
-export default function App() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-red-500">Krypthon Less go</Text>
-      <StatusBar style="auto"/>
-    </View>
-  );
-};
+
+
+export default () => {
+  return(
+  <Home />
+  )
+}
 
