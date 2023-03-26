668e84ec19da6e03e252e8d0977576da39954645
Shubham Singh <shubham212a@gmail.com>
2023-03-26 17:26:25 +0530

    added into app.js

--- App.js
+++ App.js
@@ -1,5 +1,21 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+
+import { createAppContainer , createSwitchNavigator} from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+
+import Home from "./screen/Home"
+import Profile from "./screen/Profile"
+import Appointments from "./screen/Appointment"
+import AddAppointments from "./screen/AddAppointment"
+import Patient from "./screen/Patient"
+import AddPatient from "./screen/AddPatient"
+import History from "./screen/History"
+import Critical from "./screen/Critcal"
+import CriticalProfile from "./screen/CriticalProfile"
+
 
 export default function App() {
   return (
